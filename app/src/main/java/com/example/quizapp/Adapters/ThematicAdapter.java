676e66ff5dc2a4f865a2d0727235b3bbc8d3a3ba
MuhammadAli511 <mohammadali417@outlook.com>
2023-02-12 14:08:07 +0500

    Question Page

--- app/src/main/java/com/example/quizapp/Adapters/ThematicAdapter.java
+++ app/src/main/java/com/example/quizapp/Adapters/ThematicAdapter.java
@@ -1,6 +1,8 @@
 package com.example.quizapp.Adapters;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +15,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.quizapp.Models.Subject;
 import com.example.quizapp.Models.ThematicArea;
 import com.example.quizapp.R;
+import com.example.quizapp.Student.SingleSubject;
+import com.example.quizapp.Student.SingleThematic;
 
 import java.util.List;
 
@@ -38,6 +42,21 @@ public class ThematicAdapter extends RecyclerView.Adapter<ThematicAdapter.ViewHo
         int pos = position;
         ThematicArea thematicArea = thematicAreasList.get(position);
         holder.thematicText.setText(thematicArea.getThematicAreaName());
+        Intent intent = ((Activity) context).getIntent();
+        String subjectName = intent.getStringExtra("subjectName");
+        String gradeName = intent.getStringExtra("gradeName");
+
+        holder.openThematicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, SingleThematic.class);
+                intent.putExtra("thematicArea", thematicArea.getThematicAreaName());
+                intent.putExtra("subjectName", subjectName);
+                intent.putExtra("gradeName", gradeName);
+                context.startActivity(intent);
+            }
+        });
+
 
     }
 
